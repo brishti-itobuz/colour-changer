7ee122bd82e649fe235d0301a5fae5eaf01d9b30
Brishti Saha <brishti@itobuz.com>
2024-12-26 05:05:27 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function orange() {
+  document.body.style.backgroundColor = "orange";
+}
+
+function red() {
+  document.body.style.backgroundColor = "red";
+}
+
+function violet() {
+  document.body.style.backgroundColor = "violet";
+}
+
+function blue() {
+  document.body.style.backgroundColor = "blue";
+}
